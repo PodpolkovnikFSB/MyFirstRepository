9b2fae0ac3975b10acd165cb33002669b6c6a627
PodpolkovnikFSB <105067184+PodpolkovnikFSB@users.noreply.github.com>
2022-05-06 19:14:06 +0300

    Add files via upload
    add option of last liter of name for "Ечка"

--- nameOchka.py
+++ nameOchka.py
@@ -1,10 +1,7 @@
-# Анюта -> АнютОчка
-# Алина -> АлинОчка
-# Оливия -> ОливОчка
-
 def ochka(name):
     glasnye = 'ёуеыаоэяию'
+    take_o = 'уеыао'
     list_name = list(name)
     while list_name[-1] in glasnye:
         list_name.pop(-1)
-    return ''.join(list_name) + 'Очка'
+    return ''.join(list_name) + ('Очка' if name[-1] in take_o else 'Ечка')
